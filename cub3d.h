8ff6fb80a5eb7a0ec08de19d29f0329a0ab96b38
Ivan Mac <ivan.teohjh@gmail.com>
2024-01-11 12:42:51 +0800

    added os check to header file

--- cub3d.h
+++ cub3d.h
@@ -6,20 +6,26 @@
 /*   By: jteoh <jteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 10:57:42 by jteoh             #+#    #+#             */
-/*   Updated: 2024/01/11 11:21:21 by jteoh            ###   ########.fr       */
+/*   Updated: 2024/01/11 12:41:58 by jteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <mlx.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <math.h>
 # include <string.h>
 # include <fcntl.h>
+
+# if __linux__
+#  include <X11/X.h>
+#  include "mlx_linux/mlx.h"
+# else
+#  include <mlx.h>
+# endif
 
 typedef struct s_data
 {
